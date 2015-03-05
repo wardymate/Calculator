a54b58b2aa7efec314a445ca951e7170c25f72f2
wardymate <chriswarddev@gmail.com>
2015-03-05 05:45:10 +0000

    calculator can add numbers and tests pass

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -12,35 +12,55 @@ describe ('Calculator', function (){
     });
 
     it('the 9 button changes the last digit of display to 9', function() {
-      calculator.nineButton();
+      calculator.numberButton(9);
       expect(calculator.screenDisplay).toEqual(9);
     });
 
     it('the 9 button twice changes the display to 99', function() {
-      calculator.nineButton();
-      calculator.nineButton();
+      calculator.numberButton(9);
+      calculator.numberButton(9);
       expect(calculator.screenDisplay).toEqual(99);
     });
 
     it('the 9 button three times changes the display to 999', function() {
-      calculator.nineButton();
-      calculator.nineButton();
-      calculator.nineButton();
+      calculator.numberButton(9);
+      calculator.numberButton(9);
+      calculator.numberButton(9);
       expect(calculator.screenDisplay).toEqual(999);
     });
 
-
-
+    it('displays 123456789 when 123456789 is entered', function() {
+      calculator.numberButton(1);
+      calculator.numberButton(2);
+      calculator.numberButton(3);
+      calculator.numberButton(4);
+      calculator.numberButton(5);
+      calculator.numberButton(6);
+      calculator.numberButton(7);
+      calculator.numberButton(8);
+      calculator.numberButton(9);
+      expect(calculator.screenDisplay).toEqual(123456789);
+    });
 
   });
 
-  // describe('buttons', function() {
+  describe('mathematical buttons', function() {
 
-  //   it('', function() {
+    it('the plus button resets the display to zero', function() {
+      calculator.numberButton(1);
+      calculator.plusButton();
+      expect(calculator.screenDisplay).toEqual(0);
+    });
 
-  //   });
+     it('the equals button diaplays the result ', function() {
+      calculator.numberButton(1);
+      calculator.plusButton();
+      calculator.numberButton(1);
+      calculator.equalsButton();
+      expect(calculator.screenDisplay).toEqual(2);
+    });
 
-  // });
+  });
 
 
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,11 +1,25 @@
 var Calculator = function() {
   this.screenDisplay = 0;
+  this.workingNumber = 0;
+  this.workingOPerator = "";
 };
 
-Calculator.prototype.nineButton = function(){
+Calculator.prototype.numberButton = function(number){
   if (this.screenDisplay === 0){
-    this.screenDisplay = 9;}
+    this.screenDisplay = number;}
   else {
-    this.screenDisplay = this.screenDisplay*10+9;
+    this.screenDisplay = this.screenDisplay*10+number;
   };
+};
+
+Calculator.prototype.plusButton = function() {
+  this.workingNumber = this.screenDisplay;
+  this.screenDisplay = 0;
+  this.workingOPerator = "plus";
+};
+
+Calculator.prototype.equalsButton = function() {
+  if (this.workingOPerator === "plus") {
+    this.screenDisplay = this.screenDisplay+this.workingNumber;
+  }
 };
